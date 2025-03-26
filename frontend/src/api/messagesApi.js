eb82c0429c315d9d479e523c1d20949b3ae0e82b
Olivia-Shch <165393515+Olivia-Shch@users.noreply.github.com>
2025-03-26 21:34:44 +1000

    Update messagesApi.js

--- frontend/src/api/messagesApi.js
+++ frontend/src/api/messagesApi.js
@@ -2,35 +2,33 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import prepareHeaders from '../utils/apiHelpers';
 import { getAuthApiRoute } from '../utils/routes';
 
-const messagesApi = createApi({
-  reducerPath: 'messagesApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: getAuthApiRoute('MESSAGES'),
-    prepareHeaders: (headers) => prepareHeaders(headers),
-  }),
-  tagTypes: ['Channels', 'Messages'],
+export const messagesApi = createApi({
+  reducerPath: 'messages',
+  baseQuery: fetchBaseQuery(
+    { baseUrl: apiPaths.messages(), prepareHeaders: setHeaders, tagTypes: ['Messages'] },
+  ),
+  tagTypes: ['Messages'],
   endpoints: (builder) => ({
     getMessages: builder.query({
-      query: () => '/messages',
-      providesTags: ['Messages'],
-      refetchOnMountOrArgChange: true, // Авто-обновление
+      query: () => '',
     }),
     addMessage: builder.mutation({
       query: (message) => ({
-        url: '/messages', // Явно указываем путь
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
         body: message,
       }),
-      invalidatesTags: ['Messages'],
+    }),
+    removeMessage: builder.mutation({
+      query: (id) => ({
+        method: 'DELETE',
+        url: id,
+      }),
     }),
   }),
 });
 
-export default messagesApi;
 export const {
   useGetMessagesQuery,
   useAddMessageMutation,
+  useRemoveMessageMutation,
 } = messagesApi;
