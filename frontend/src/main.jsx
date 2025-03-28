ce9cf2ce9b337214140c3b69d0213101b042ab1c
Olivia-Shch <165393515+Olivia-Shch@users.noreply.github.com>
2025-03-28 22:28:57 +1000

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './index.jsx'; // Импортируем приложение
+import App from './index.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
