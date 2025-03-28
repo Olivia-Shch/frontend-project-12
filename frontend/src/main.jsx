c33540cdb92d0b381c5b59d7213a1c6b5d0d2714
Olivia-Shch <165393515+Olivia-Shch@users.noreply.github.com>
2025-03-28 21:39:26 +1000

    review_corr - update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { io } from 'socket.io-client';
 import init from './init.jsx';
@@ -13,11 +13,9 @@ const socket = io('/socket.io', {
 const RootComponent = () => {
   const [app, setApp] = useState(null);
 
-  useEffect(() => {
-    init(socket).then(setApp).catch((err) => {
-      console.error('Ошибка при инициализации:', err);
-    });
-  }, []);
+  init(socket).then(setApp).catch((err) => {
+    console.error('Ошибка при инициализации:', err);
+  });
 
   return app || <div>Loading...</div>;
 };
