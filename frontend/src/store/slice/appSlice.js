ce68614b165c4c64f6d10acfd48aa8f1e1f32284
Olivia-Shch <165393515+Olivia-Shch@users.noreply.github.com>
2025-03-28 22:32:10 +1000

    review_corr - update appSlice.js

--- frontend/src/store/slice/appSlice.js
+++ frontend/src/store/slice/appSlice.js
@@ -1,17 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
-import channelsApi from '../../api/channelsApi';
-import messagesApi from '../../api/messagesApi';
 
 const initialState = {
   currentChannelId: 1,
   currentChannelName: 'general',
-  message: '',
   modalType: '',
   channelId: '',
   channelName: '',
-  isError: false,
-  error: null,
-  isOpen: false,
 };
 
 const appSlice = createSlice({
@@ -19,118 +13,23 @@ const appSlice = createSlice({
   initialState,
   reducers: {
     changeChannel: (state, { payload }) => {
-      Object.assign(state, {
-        currentChannelId: payload.id,
-        currentChannelName: payload.name,
-      });
+      state.currentChannelId = payload.id;
+      state.currentChannelName = payload.name;
     },
     setChannelModal: (state, { payload }) => {
-      Object.assign(state, {
-        modalType: payload.modalName,
-        isOpen: true,
-        channelId: payload.id,
-        channelName: payload.name,
-      });
+      state.modalType = payload.modalName;
+      state.channelId = payload.id;
+      state.channelName = payload.name;
     },
     setDefaultChannel: (state) => {
-      Object.assign(state, {
-        currentChannelId: 1,
-        currentChannelName: 'general',
-      });
+      state.currentChannelId = 1;
+      state.currentChannelName = 'general';
     },
     closeModal: (state) => {
-      Object.assign(state, {
-        modalType: '',
-        channelId: '',
-        channelName: '',
-        error: null,
-        isError: false,
-      });
+      state.modalType = '';
+      state.channelId = '';
+      state.channelName = '';
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addMatcher(channelsApi.endpoints.addChannel.matchPending, (state) => {
-        Object.assign(state, {
-          isError: false,
-          error: null,
-          isOpen: true,
-        });
-      })
-      .addMatcher(channelsApi.endpoints.addChannel.matchFulfilled, (state, { payload }) => {
-        const { name, id } = payload;
-        Object.assign(state, {
-          currentChannelName: name,
-          currentChannelId: id,
-          error: null,
-          isOpen: false,
-        });
-      })
-      .addMatcher(channelsApi.endpoints.addChannel.matchRejected, (state, { payload }) => {
-        Object.assign(state, {
-          isError: true,
-          error: payload.status,
-        });
-      })
-      .addMatcher(channelsApi.endpoints.removeChannel.matchPending, (state) => {
-        Object.assign(state, {
-          isError: false,
-          error: null,
-          isOpen: true,
-        });
-      })
-      .addMatcher(channelsApi.endpoints.removeChannel.matchFulfilled, (state) => {
-        Object.assign(state, {
-          error: null,
-          isOpen: false,
-        });
-      })
-      .addMatcher(channelsApi.endpoints.removeChannel.matchRejected, (state, { payload }) => {
-        Object.assign(state, {
-          isError: true,
-          error: payload.status,
-        });
-      })
-      .addMatcher(channelsApi.endpoints.editChannel.matchPending, (state) => {
-        Object.assign(state, {
-          isError: false,
-          error: null,
-          isOpen: true,
-        });
-      })
-      .addMatcher(channelsApi.endpoints.editChannel.matchFulfilled, (state, { payload }) => {
-        const { name, id } = payload;
-        Object.assign(state, {
-          currentChannelName: name,
-          currentChannelId: id,
-          error: null,
-          isOpen: false,
-        });
-      })
-      .addMatcher(channelsApi.endpoints.editChannel.matchRejected, (state, { payload }) => {
-        Object.assign(state, {
-          isError: true,
-          error: payload.status,
-        });
-      })
-      .addMatcher(messagesApi.endpoints.addMessage.matchPending, (state) => {
-        Object.assign(state, {
-          error: null,
-        });
-      })
-      .addMatcher(messagesApi.endpoints.addMessage.matchFulfilled, (state, { payload }) => {
-        const { message } = payload;
-        Object.assign(state, {
-          error: null,
-          message,
-        });
-      })
-      .addMatcher(messagesApi.endpoints.addMessage.matchRejected, (state, { payload }) => {
-        Object.assign(state, {
-          isError: true,
-          error: payload.status,
-        });
-      });
   },
 });
 
@@ -143,11 +42,8 @@ export const {
 
 export const selectCurrentChannelId = (state) => state.app.currentChannelId;
 export const selectCurrentChannelName = (state) => state.app.currentChannelName;
-export const selectIsError = (state) => state.app.isError;
 export const selectChannelName = (state) => state.app.channelName;
 export const selectChannelId = (state) => state.app.channelId;
 export const selectModalType = (state) => state.app.modalType;
-export const selectError = (state) => state.app.error;
-export const selectIsOpen = (state) => state.app.isOpen;
 
 export default appSlice.reducer;
